973fd01ced8fbb1ec4439d80339263675703d8d5
AShalaby2004 <abdelrahmanshalaby4004@gmail.com>
2024-11-14 19:37:26 +0200

    OK

--- Test.py
+++ Test.py
@@ -1,8 +1,10 @@
 import unittest
-from Add import calc
+from Add import add
+
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-        result =calc.add(4,6)
-        self.assertEqual(result,10)
-if _name== 'main_':
-    unittest.main
+        result = add.add(4, 6)
+        self.assertEqual(result, 10)
+
+if __name__ == '__main__':
+    unittest.main()
